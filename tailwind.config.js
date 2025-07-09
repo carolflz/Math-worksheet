c03c268f7254a5643488d8ed62b38c47f8212ae2
Foo Ling Zhen <lzhen803@gmail.com>
2025-07-09 17:54:38 +0800

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'], // ✅ required
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
